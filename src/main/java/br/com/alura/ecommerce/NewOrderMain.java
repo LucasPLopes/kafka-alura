2561bdf98ebdbca29b7cada11bb509fa1ef1b4c2
lucas <l.lopes.fga@gmail.com>
2022-05-08 15:54:20 -0300

    view consumer-groups in action

--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -14,21 +14,21 @@ public class NewOrderMain {
     private static final Logger log = LoggerFactory.getLogger(NewOrderMain.class);
 
     public static void main(String[] args) {
+        for (int i = 0; i < 100; i++)
+            try (var producer = new KafkaProducer<String, String>(PropertiesConfig.producerProperties())) {
+                var key = UUID.randomUUID().toString();
+                var value = String.format("PEDIDO%S;%.2f", key, Math.random() % 150);
+                var record = new ProducerRecord<>("LOJA_NOVO_PEDIDO", key, value);
+                var record2 = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
+                var email = "Thank you for your order! We are processing your order!";
+                var record3 = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
 
-        try (var producer = new KafkaProducer<String, String>(PropertiesConfig.producerProperties())) {
-            var key = UUID.randomUUID().toString();
-            var value = String.format("PEDIDO%S;%.2f", key, Math.random() % 150);
-            var record = new ProducerRecord<>("LOJA_NOVO_PEDIDO", key, value);
-            var record2 = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
-            var email = "Thank you for your order! We are processing your order!";
-            var record3 = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
-
-            producer.send(record, producerCallback()).get();
-            producer.send(record2, producerCallback()).get();
-            producer.send(record3, producerCallback()).get();
-        } catch (Exception e) {
-            log.error("NewOrderMain error: {}", e.getMessage());
-        }
+                producer.send(record, producerCallback()).get();
+                producer.send(record2, producerCallback()).get();
+                producer.send(record3, producerCallback()).get();
+            } catch (Exception e) {
+                log.error("NewOrderMain error: {}", e.getMessage());
+            }
 
     }
 
